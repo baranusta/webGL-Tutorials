d7d9bcd9823039deb47bbc7e3e3505877ec0c0f5
Baran Usta <baranusta@sabanciuniv.edu>
2017-10-10 14:33:57 +0300

    implements game logic

--- player.js
+++ player.js
@@ -2,20 +2,39 @@ class Player {
     
     constructor( name, center, width, height ) {
         this.name = name;
-        this.x = 0;
+        this.displacement = [0.0,0.0];
+        this.speedY = 0.0;
         this.score = 0;
 
         this.program = initShaders( gl, 'vertex-shader', 'fragment-shader' );
-        this.program.x = gl.getUniformLocation( this.program, 'time' );
+        this.program.displacement = gl.getUniformLocation( this.program, 'displacement' );
 
         this.rectangle = new Rectangle( center, width, height, this.program );
     }
 
     draw() {
-        this.rectangle.draw( this.x );
+        this.rectangle.draw( this.displacement );
     }
 
     update() {
-        this.x += 0.01;
+        //console.log(this.displacement);
+        this.displacement[1] += this.speedY;
+    }
+
+    accelerate(up){
+        if(up){
+            if(this.speedY <0.01 )
+                this.speedY += 0.001;
+        }
+        else{
+            if(this.speedY > -0.01)
+            this.speedY -= 0.001;    
+        } 
+        console.log(this.speedY);
+        
+    }
+
+    decelerate(){
+        this.speedY *= 0.8;
     }
 }
