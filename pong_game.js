104e7ab06f016c0798ad2819c8d0a9ff937a2585
Baran Usta <baranusta@sabanciuniv.edu>
2017-10-10 14:35:15 +0300

    Implements game logic

--- pong_game.js
+++ pong_game.js
@@ -1,39 +1,82 @@
 
 let player_1, player_2;
 
+let ball;
+let screenSize;
+let gameEnded = false;
+
+var playerPressedU = false;
+var playerPressedD = false;
+
 window.onload = function () {
     let canvas = document.getElementById("gl-canvas");
     gl = WebGLUtils.setupWebGL(canvas);
     if (!gl) {
         alert("yo");
     }
-
+    screenSize = [canvas.width, canvas.height];
 
     gl.viewport(0, 0, canvas.width, canvas.height);
     gl.clearColor(0, 1.0, 0, 1);
 
-    player_1 = new Player( "P_1", vec2(0, 0), 0.15, 0.15 );
-    player_2 = new Player( "P_2", vec2(0, 0), 0.25, 0.25 );
-
+    player_1 = new Player( "P_1", vec2(-.85, 0), 0.15, 0.2 );
+    player_2 = new Player( "P_2", vec2(+.85, 0), 0.15, 0.2 );
+    
+    ball = new Ball("ball", vec2(0,0),0.1);
     gameLoop();
 }
 
-let gameEnded = false;
 const gameLoop = function(){
     gl.clear(gl.COLOR_BUFFER_BIT);
     
     if(!gameEnded)
     {
+        // console.log(playerPressedU)
+        // console.log(playerPressedD)
+        if(!playerPressedU && !playerPressedD )
+            player_1.decelerate();
+        else 
+            player_1.accelerate(playerPressedU);
+
+
         player_1.update();
         player_2.update();
 
         player_1.draw();
         player_2.draw();
-
+        //ball.draw(screenSize);
         window.requestAnimationFrame(gameLoop);
     }
     else
     {
         window.cancelAnimationFrame(gameLoop);
     }
+}
+
+
+document.onkeydown = function(e)
+{
+    console.log(e.keyCode);
+    if(e.keyCode == 38){
+        playerPressedU = true;
+    }
+    if(e.keyCode == 40){
+        playerPressedD = true;
+    }
+    // console.log(playerPressedU)
+    // console.log(playerPressedD)
+    
+}
+
+document.onkeyup = function(e)
+{
+    if(e.keyCode == 38){
+        playerPressedU = false;
+    }
+    if(e.keyCode == 40){        
+        playerPressedD = false;
+    }
+    
+    // console.log(playerPressedU)
+    // console.log(playerPressedD)
 }
